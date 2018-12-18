46ceb13ff975e3f8e1e0ec403d4a31d7d9f58139
Pol Gómez Guerrero <pol@sheddapp.com>
2018-12-18 23:27:57 +0100

    Movie list adapter almost finished, created view holder and listener

--- app/src/main/java/com/polgomez/core/extensions/ViewExt.kt
+++ app/src/main/java/com/polgomez/core/extensions/ViewExt.kt
@@ -1,6 +1,9 @@
 package com.polgomez.core.extensions
 
+import android.support.annotation.LayoutRes
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 
 fun View.show() {
     this.visibility = View.VISIBLE
@@ -9,3 +12,6 @@ fun View.show() {
 fun View.hide() {
     this.visibility = View.GONE
 }
+
+fun ViewGroup.inflate(@LayoutRes layoutRes: Int): View =
+    LayoutInflater.from(context).inflate(layoutRes, this, false)
--- app/src/main/java/com/polgomez/movies/list/view/adapter/MoviesListAdapter.kt
+++ app/src/main/java/com/polgomez/movies/list/view/adapter/MoviesListAdapter.kt
@@ -1,35 +1,58 @@
 package com.polgomez.movies.list.view.adapter
 
 import android.support.v7.widget.RecyclerView
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.polgomez.core.extensions.inflate
 import com.polgomez.core.view.ImageLoader
+import com.polgomez.movies.R
+import com.polgomez.movies.model.MovieModel
 
-class MoviesListAdapter(private val imageLoader: ImageLoader) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class MoviesListAdapter(private val imageLoader: ImageLoader) : RecyclerView.Adapter<MovieViewHolder>() {
 
-    private var movies: MutableList<Any> = ArrayList()
+    private var movies: MutableList<MovieModel> = ArrayList()
+
+    private lateinit var movieListener: (MovieModel) -> Unit
 
     init {
         setHasStableIds(true)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder =
+        MovieViewHolder(parent.inflate(viewType))
 
     override fun getItemCount(): Int = movies.size
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getItemViewType(position: Int): Int = R.layout.item_movie
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) = with(holder) {
+        val movieModel = movies[position]
+        imageLoader.loadImage(movieImageView, movieModel.imageUrl)
+        titleText.text = movieModel.title
+        itemView.setOnClickListener {
+            movieListener.invoke(movieModel)
+        }
     }
 
-    fun setMovies(movies: List<Any>) {
+    fun setMovieClickListener(listener: (MovieModel) -> Unit) {
+        movieListener = listener
+    }
+
+    fun setMovies(movies: List<MovieModel>) {
         this.movies = movies.toMutableList()
         notifyDataSetChanged()
     }
 
-    fun addMovies(movies: List<Any>) {
+    fun addMovies(movies: List<MovieModel>) {
         val previousSize = this.movies.size
         this.movies.addAll(movies)
         notifyItemRangeInserted(previousSize, this.movies.size)
     }
+}
+
+class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val movieImageView: ImageView = itemView.findViewById(R.id.movieImageView)
+    val titleText: TextView = itemView.findViewById(R.id.titleText)
 }
