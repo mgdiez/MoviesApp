cd51f3454a7393c014db60dbd1dc1df6f6d39324
Pol Gómez Guerrero <pol@sheddapp.com>
2018-12-18 15:18:31 +0100

    Added splash theme to activity

--- app/src/main/java/com/polgomez/movies/MainActivity.kt
+++ app/src/main/java/com/polgomez/movies/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.polgomez.movies
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
