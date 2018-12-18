ed58c4360ce5b1d8a4fd5f3548b4fc67791e38d9
Pol Gómez Guerrero <pol@sheddapp.com>
2018-12-18 14:28:54 +0100

    Created an Application class and DI

---
+++ app/src/main/java/com/polgomez/movies/app/di/MoviesAppComponent.kt
@@ -0,0 +1,10 @@
+package com.polgomez.movies.app.di
+
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [MoviesAppModule::class])
+interface MoviesAppComponent {
+
+}
---
+++ app/src/main/java/com/polgomez/movies/app/di/MoviesAppModule.kt
@@ -0,0 +1,9 @@
+package com.polgomez.movies.app.di
+
+import com.polgomez.movies.app.MoviesApp
+import dagger.Module
+
+@Module
+class MoviesAppModule(private val moviesApp: MoviesApp) {
+
+}
