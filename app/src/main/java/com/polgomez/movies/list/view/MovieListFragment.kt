740a9128d75720a0cb9e1f92502cce2ed676b2a1
Pol Gómez Guerrero <pol@sheddapp.com>
2018-12-18 19:19:23 +0100

    Created view function extensions, handling error visibility

--- app/src/main/java/com/polgomez/movies/list/view/MovieListFragment.kt
+++ app/src/main/java/com/polgomez/movies/list/view/MovieListFragment.kt
@@ -2,11 +2,22 @@ package com.polgomez.movies.list.view
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.polgomez.core.extensions.hide
+import com.polgomez.core.extensions.show
 import com.polgomez.movies.MoviesActivity
+import com.polgomez.movies.R
 import com.polgomez.movies.list.MoviesListContract
 import com.polgomez.movies.list.di.MoviesListModule
+import kotlinx.android.synthetic.main.fragment_movies_list.*
+import javax.inject.Inject
 
 class MovieListFragment : Fragment(), MoviesListContract.View {
+
+    @Inject
+    private lateinit var presenter: MoviesListContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +29,21 @@ class MovieListFragment : Fragment(), MoviesListContract.View {
         val moviesActivity = activity as MoviesActivity
         moviesActivity.moviesActivityComponent.moviesListComponentBuilder().moviesListModule(MoviesListModule()).build()
             .inject(this)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        inflater.inflate(R.layout.fragment_movies_list, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initializeViews()
+    }
+
+    private fun initializeViews() {
+        retryButton.setOnClickListener {
+            presenter.onRetryClicked()
+        }
     }
 
     override fun showMovies(movies: List<*>) {
@@ -32,11 +58,9 @@ class MovieListFragment : Fragment(), MoviesListContract.View {
     override fun hideLoading() {
     }
 
-    override fun showError() {
-    }
+    override fun showError() = errorLayout.show()
 
-    override fun hideError() {
-    }
+    override fun hideError() = errorLayout.hide()
 
     companion object {
 
