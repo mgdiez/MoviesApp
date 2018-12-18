ee3ff851ef97be34dcb6898ce9317ada4f577275
Pol Gómez Guerrero <pol@sheddapp.com>
2018-12-18 17:46:37 +0100

    Defined movie liust contract

---
+++ app/src/main/java/com/polgomez/movies/list/MoviesListContract.kt
@@ -0,0 +1,31 @@
+package com.polgomez.movies.list
+
+interface MoviesListContract {
+    interface View {
+        fun showError()
+        fun showMovies(movies: List<*>)
+        fun addMovies(movies: List<*>)
+        fun showLoading()
+        fun hideLoading()
+        fun hideError()
+    }
+
+    interface Presenter {
+        fun start()
+        fun stop()
+        fun onMovieClicked()
+        fun onBottomReached()
+        fun onRetryClicked()
+    }
+
+    interface State {
+        fun getMovies(): List<*>
+        fun setMovies(movies: List<*>)
+        fun setPage(page: Int)
+        fun getPage(): Int
+    }
+
+    interface Navigation {
+        fun onMovieSelected()
+    }
+}
