c9aec545cb2af6905f3be44999be9465149a40b5
Pol Gómez Guerrero <pol@sheddapp.com>
2018-12-19 21:59:34 +0100

    Added movie placeholder

--- app/src/main/java/com/polgomez/core/view/ImageLoader.kt
+++ app/src/main/java/com/polgomez/core/view/ImageLoader.kt
@@ -1,6 +1,7 @@
 package com.polgomez.core.view
 
 import android.widget.ImageView
+import com.polgomez.movies.R
 import com.squareup.picasso.Picasso
 
 interface ImageLoader {
@@ -8,8 +9,11 @@ interface ImageLoader {
     fun loadImage(imageView: ImageView, url: String)
 }
 
-class PicassoImageLoader : ImageLoader {
-    override fun loadImage(imageView: ImageView, url: String) {
-        Picasso.get().load(url).into(imageView)
+class PicassoMovieImageLoader : ImageLoader {
+    override fun loadImage(
+        imageView: ImageView,
+        url: String
+    ) {
+        Picasso.get().load(url).placeholder(R.drawable.movie_placeholder).into(imageView)
     }
 }
--- app/src/main/java/com/polgomez/movies/app/di/MoviesAppModule.kt
+++ app/src/main/java/com/polgomez/movies/app/di/MoviesAppModule.kt
@@ -1,7 +1,7 @@
 package com.polgomez.movies.app.di
 
 import com.polgomez.core.view.ImageLoader
-import com.polgomez.core.view.PicassoImageLoader
+import com.polgomez.core.view.PicassoMovieImageLoader
 import com.polgomez.movies.app.MoviesApp
 import dagger.Module
 import dagger.Provides
@@ -16,7 +16,7 @@ open class MoviesAppModule(private val moviesApp: MoviesApp) {
 
     @Provides
     @Singleton
-    fun provideImageLoader(): ImageLoader = PicassoImageLoader()
+    fun provideImageLoader(): ImageLoader = PicassoMovieImageLoader()
 
     @Provides
     @Singleton
